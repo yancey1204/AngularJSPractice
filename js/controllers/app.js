baf13e9fe511fb972164eb53595ce07a06efeb1e
yancey <yancey1204@gmail.com>
2015-05-24 00:15:18 +0200

    edit the styling

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -3,9 +3,11 @@ var app = angular.module('practiceApp',[]);
 app.controller('SwitchController',['$scope',function($scope){
     $scope.items =['AGENTS','HELP'];
     $scope.selection = $scope.items[0];
+    $scope.itemIndex = 0;
 
     $scope.switch= function(index){
         $scope.selection = $scope.items[index];
+        $scope.itemIndex = index;
     };
-
 }]);
+
